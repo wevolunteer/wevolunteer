11a702015de2ea12afe920d9f3736a8cd21b05d0
Jonathan Mataloni <jo.mataloni@gmail.com>
2024-08-09 13:36:09 +0200

    feat(adm): org details

--- admin/src/pages/organizations/edit.tsx
+++ admin/src/pages/organizations/edit.tsx
@@ -1,9 +1,8 @@
 import { Edit, useForm } from "@refinedev/antd";
-import { Form, Input } from "antd";
+import { Form, Input, Switch } from "antd";
 
 export const OrganizationEdit = () => {
   const { formProps, saveButtonProps, formLoading } = useForm({});
-
 
   return (
     <Edit saveButtonProps={saveButtonProps} isLoading={formLoading}>
@@ -19,21 +18,10 @@ export const OrganizationEdit = () => {
         >
           <Input />
         </Form.Item>
-        <Form.Item
-          label={"Email"}
-          name={["email"]}
-        >
-          <Input />
-        </Form.Item>
+
         <Form.Item
           label={"Phone"}
           name={["phone"]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={"ID Esterno"}
-          name={["external_id"]}
           rules={[
             {
               required: true,
@@ -43,6 +31,65 @@ export const OrganizationEdit = () => {
           <Input />
         </Form.Item>
 
+        <Form.Item
+          label={"Email"}
+          name={["email"]}
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={"WebSite"} name={["website"]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={"Address"} name={["address"]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={"City"} name={["city"]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={"State"} name={["state"]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={"ZipCode"} name={["zip_code"]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={"Country"} name={["country"]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={"Latitude"} name={["latitude"]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={"Longitude"} name={["longitude"]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={"TaxCode"} name={["tax_code"]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={"VATCode"} name={["vat_code"]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={"ExternalId"} name={["external_id"]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item label={"Published"} name={["published"]}>
+          <Switch />
+        </Form.Item>
       </Form>
     </Edit>
   );
--- admin/src/pages/organizations/show.tsx
+++ admin/src/pages/organizations/show.tsx
@@ -1,8 +1,4 @@
-import {
-  NumberField,
-  Show,
-  TextField
-} from "@refinedev/antd";
+import { NumberField, Show, TextField } from "@refinedev/antd";
 import { useShow } from "@refinedev/core";
 import { Typography } from "antd";
 
@@ -14,13 +10,40 @@ export const OrganizationShow = () => {
 
   const record = data?.data;
 
-
   return (
     <Show isLoading={isLoading}>
       <Title level={5}>{"ID"}</Title>
       <NumberField value={record?.id ?? ""} />
       <Title level={5}>{"Name"}</Title>
       <TextField value={record?.name} />
+      <Title level={5}>{"Phone"}</Title>
+      <TextField value={record?.phone} />
+      <Title level={5}>{"Email"}</Title>
+      <TextField value={record?.email} />
+      <Title level={5}>{"WebSite"}</Title>
+      <TextField value={record?.website} />
+      <Title level={5}>{"Address"}</Title>
+      <TextField value={record?.address} />
+      <Title level={5}>{"City"}</Title>
+      <TextField value={record?.city} />
+      <Title level={5}>{"State"}</Title>
+      <TextField value={record?.state} />
+      <Title level={5}>{"ZipCode"}</Title>
+      <TextField value={record?.zip_code} />
+      <Title level={5}>{"Country"}</Title>
+      <TextField value={record?.country} />
+      <Title level={5}>{"Latitude"}</Title>
+      <TextField value={record?.latitude} />
+      <Title level={5}>{"Longitude"}</Title>
+      <TextField value={record?.longitude} />
+      <Title level={5}>{"TaxCode"}</Title>
+      <TextField value={record?.tax_code} />
+      <Title level={5}>{"VATCode"}</Title>
+      <TextField value={record?.vat_code} />
+      <Title level={5}>{"Published"}</Title>
+      <TextField value={record?.published ? "Yes" : "No"} />
+      <Title level={5}>{"ExternalId"}</Title>
+      <TextField value={record?.external_id ?? ""} />
     </Show>
   );
 };
