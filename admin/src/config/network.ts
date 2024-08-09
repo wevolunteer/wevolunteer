b920e142f77f149640936b3673b6c88af564a1b2
Jonathan Mataloni <jo.mataloni@gmail.com>
2024-08-09 17:37:36 +0200

    ci(adm): prepare for production

--- admin/src/config/network.ts
+++ admin/src/config/network.ts
@@ -2,7 +2,11 @@ import axios from "axios";
 import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "../authProvider";
 
 export function getApiEndpoint() {
-  return "http://localhost:3000/api";
+  if (process.env.NODE_ENV === "production") {
+    return "https://wevolunteer.app/api";
+  } else {
+    return "http://localhost:3000/api";
+  }
 }
 
 export function getAxiosInstance() {
