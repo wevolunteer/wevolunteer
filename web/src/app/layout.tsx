f6265a6e7eb60504765d2a9d81caf46ac7259c34
Fabrizio Arzeni <fabrizio.arz@gmail.com>
2024-10-14 15:19:51 +0200

    fix: update landing page font

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Attivati!",
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={dmSans.className}>
         <Header />
         {children}
         <Footer />
